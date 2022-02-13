34ef3349b3ec9fdec3bd4beef8e5f5957cebdcd6
Igor Tkachenko <stiles.2126@gmail.com>
2022-02-13 18:09:57 +1000

    feat: create swagger documentation microservice

---
+++ README.md
@@ -0,0 +1,5 @@
+## Description
+
+Main microservice for Swagger documentation
+
+#### To see the documentation go to the route: `http://localhost/api/docs/`
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+
+    const config = new DocumentBuilder()
+        .setTitle('Swagger Service')
+        .setDescription('Main microservice for documentation')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document, {
+        explorer: true,
+        swaggerOptions: {
+            urls: [
+                {
+                    url: `http://localhost/api/auth/docs-json`,
+                    name: 'AUTH-MICROSERVICE',
+                },
+                {
+                    url: `http://localhost/api/users/docs-json`,
+                    name: 'USERS-MICROSERVICE',
+                },
+            ],
+        },
+    });
+
+    await app.listen(3000);
+}
+bootstrap();
